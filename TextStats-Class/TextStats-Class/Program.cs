d83326ba59dbd1f4914fb022ad6ff0959f74b062
Dustin Kraft <kraftdr@gmail.com>
2014-06-24 14:11:20 -0600

    Adding Text Stats Code

--- TextStats-Class/TextStats-Class/Program.cs
+++ TextStats-Class/TextStats-Class/Program.cs
@@ -10,6 +10,60 @@ namespace TextStats_Class
     {
         static void Main(string[] args)
         {
+            TextStats("who is your daddy and what does he do?");
+            TextStats("i dont know.  some words here.  things too.  I like stuff and things.");
+            //Keep the console open
+            Console.ReadKey();
+        }
+
+        static void TextStats(string input)
+        {
+            //display the original input
+            Console.WriteLine("Original Input: " + input);
+            //format my string to be all lowercase
+            input = input.ToLower();
+
+            //number of characters
+            Console.WriteLine("# of Characters: " + input.Length);
+            //number of words
+            Console.WriteLine("# of Words: " + input.Split(' ').Length);
+
+            // declaring variables to hold the number
+            // of each type of character
+            int numVowels = 0;
+            int numCons = 0;
+            int specChars = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                //grabbing one letter from the string
+                // by it's index.
+                char letter = input[i];
+                //number of vowels
+                //Is it a vowel?
+                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+                {
+                    //found a vowel! add 1 to the vowel count
+                    numVowels = numVowels + 1;
+                }
+                else if (letter == '.' || letter == ' ')
+                {
+                    //number of special characters
+                    //found a spec char, add 1 to our counter
+                    specChars = specChars + 1;
+                }
+                else
+                {
+                    //number of consonants
+                    //assume we found a consonant, 
+                    //add 1 to our counter
+                    numCons = numCons + 1;
+                }   
+            }
+            //Output number of vowels, cons,
+            // and spec chars
+            Console.WriteLine("# of Vowels: " + numVowels);
+            Console.WriteLine("# of Consonants: " + numCons);
+            Console.WriteLine("# of Spec Chars: " + specChars);
         }
     }
 }
