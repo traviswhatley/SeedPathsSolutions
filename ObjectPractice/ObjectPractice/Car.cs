857ccb2221749e42557613eb1d686dfc396795a7
Dustin Kraft <kraftdr@gmail.com>
2014-07-02 11:11:20 -0600

    Object Practice

---
+++ ObjectPractice/ObjectPractice/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPractice
+{
+    class Car
+    {
+        //Properties - define attributes of an object
+        public string Make { get; set; }
+        public string Model { get; set; }
+        private int Year { get; set; }
+
+        //Constructors come next
+        public Car(string make, string model, int year)
+        {
+            this.Make = make; this.Model = model;
+            this.Year = year;
+        }
+        
+        //Default constructor (optional)
+        public Car()
+        {
+            this.Make = "undefined";
+            this.Model = "undefined";
+        }
+
+        //Functions come after constructors
+        public string GetInfo()
+        {
+            return this.Year + " " + this.Make + " " + this.Model;
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine("Beep beep!");
+        }
+    }
+}
---
+++ ObjectPractice/ObjectPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Car1 = new Car("Honda", "Civic", 1998);
+            Car Car2 = new Car();
+            
+            Console.WriteLine(Car1.GetInfo());
+            Console.WriteLine(Car2.GetInfo());
+
+            Car2.Make = "Ford";
+            Car2.Model = "Taurus";
+
+            Car1.Honk();
+            
+            Console.WriteLine(Car2.GetInfo());
+            Console.ReadKey();
+        }
+    }
+}
