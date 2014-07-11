51a08406f9b6ffbf95fec747ff7e7fc071f5c470
Dustin Kraft <kraftdr@gmail.com>
2014-07-11 16:54:44 -0600

    Added Portfolio Site

---
+++ PortfolioSite-Class/PortfolioSite-Class/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioSite_Class.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Home/About
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        // GET: /Home/Work
+        public ActionResult Work()
+        {
+            return View();
+        }
+
+        // GET: /Home/Contact
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            //create a new contact form object to 
+            // pass to our view
+            Models.ContactForm cf = new Models.ContactForm();
+            return View(cf);
+        }
+
+        // POST: /Home/Contact
+        [HttpPost]
+        public ActionResult Contact(Models.ContactForm cf)
+        {
+            //create a new connection to our database
+            Models.ContactFormEntities db = new Models.ContactFormEntities();
+            //add the contact info to the database
+            db.ContactForms.Add(cf);
+            //save changes to the db
+            db.SaveChanges();
+            //Redirect user to the thank you page
+            return RedirectToAction("ThankYou");
+        }
+
+        // GET: /Home/ThankYou
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+}
---
+++ PortfolioSite-Class/PortfolioSite-Class/Models/ContactFormValidation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//add using statement for data annotations
+using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioSite_Class.Models
+{
+    //add the metadata type to the contact form
+    [MetadataType(typeof(ContactFormValidation))]
+    public partial class ContactForm
+    {
+        //nothing goes here
+    }
+
+    public class ContactFormValidation
+    {
+        //data annotations handle the label display of our
+        // fields and set the requirments for valid data.
+        [Required(ErrorMessage="You need a name")
+        , Display(Name="Your Name")
+        , MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(50)
+        ,Display(Name="Company Name")]
+        public string CompanyName { get; set; }
+
+        [Required
+        , EmailAddress(ErrorMessage="Your email need to be something@something.com")
+        ,MaxLength(50)]
+        public string Email { get; set; }
+
+        [Required
+        ,DataType(DataType.MultilineText)
+        ,MaxLength(500)]
+        public string Comments { get; set; }
+    }
+}
