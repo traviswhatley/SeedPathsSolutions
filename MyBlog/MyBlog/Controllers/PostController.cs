0e344bad30b9b5f6d859c1e8cc5cc764c5b26076
Dustin Kraft <kraftdr@gmail.com>
2014-07-18 15:45:11 -0600

    Blog Updates

--- MyBlog/MyBlog/Controllers/PostController.cs
+++ MyBlog/MyBlog/Controllers/PostController.cs
@@ -57,5 +57,55 @@ namespace MyBlog.Controllers
 
         }
 
+        //GET: /Posts/Delete/1
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            //get the post from the database
+            Models.Post postToDelete = db.Posts.Find(id);
+            //pass the object to the view
+            return View(postToDelete);
+        }
+
+        //POST: /Post/Delete/1
+        //Using ActionName("Delete") lets have still use
+        // the URL of /Post/Delete, while having a different
+        // action name
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            //get the post from the database
+            Models.Post postToDelete = db.Posts.Find(id);
+            //delete the post from the database
+            db.Posts.Remove(postToDelete);
+            //save the changes to the DB
+            db.SaveChanges();
+            //redirect the user back to their post listing
+            return RedirectToAction("Index", "Post");
+        }
+
+        //GET: /Post/Edit/1
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            //get the post to edit from the db
+            Models.Post postToEdit = db.Posts.Find(id);
+            //pass our post to edit to the view
+            return View(postToEdit);
+        }
+
+        //POST: /Post/Edit/1
+        [HttpPost]
+        public ActionResult Edit(Models.Post postToEdit)
+        {
+            //set the post to be updated
+            db.Entry(postToEdit).State = System.Data.EntityState.Modified;
+            //save our changes
+            db.SaveChanges();
+            //kick the back to their post list
+            return RedirectToAction("Index", "Post");
+        }
+
+
     }
 }
