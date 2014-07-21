98ef815478dbdb8172f8d46ac9ce77e9bd35cc34
Dustin Kraft <kraftdr@gmail.com>
2014-07-21 13:56:46 -0600

    Week5CodeChallenge

---
+++ Week5CodeChallenge/Week5CodeChallenge/Controllers/AboutController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Week5CodeChallenge.Controllers
+{
+    public class AboutController : Controller
+    {
+        //
+        // GET: /About/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Who()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            return View();
+        }
+
+        public ActionResult What()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            return View();
+        }
+
+        public ActionResult Where()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            return View();
+        }
+
+        public ActionResult Why()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            return View();
+        }
+
+        public ActionResult How()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            return View();
+        }
+    }
+}
