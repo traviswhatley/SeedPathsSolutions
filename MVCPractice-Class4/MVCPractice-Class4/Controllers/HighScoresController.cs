2b5c2e1579afb845f85e7ae018590b6a8c9c85f4
Dustin Kraft <kraftdr@gmail.com>
2014-09-24 15:33:08 -0600

    added mvc practice

---
+++ MVCPractice-Class4/MVCPractice-Class4/Controllers/HighScoresController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//bring in the objects
+using MVCPractice_Class4.Models;
+
+namespace MVCPractice_Class4.Controllers
+{
+    public class HighScoresController : Controller
+    {
+        //
+        // GET: /HighScores/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GuessThatNumber()
+        {
+            //set up a connection to the database
+            HighScoresEntities db = new HighScoresEntities();
+            //pass the list of high scores for GTN to
+            // the view
+            return View(db.HighScores.Where(x=> x.Game == "Guess That Number").OrderBy(x=> x.Score));
+        }
+
+    }
+}
---
+++ MVCPractice-Class4/MVCPractice-Class4/Models/HighScores.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\Admin\Documents\GitHub\SeedPathsSolutions\MVCPractice-Class4\MVCPractice-Class4\Models\HighScores.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
