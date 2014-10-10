18129e59e81e5599c4620b9222ff135ee5cca46d
Dustin Kraft <kraftdr@gmail.com>
2014-10-09 19:54:22 -0600

    clean up

---
+++ MyBlog-Class4/MyBlog-Class4/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlog_Class4.Controllers
+{
+    public class HomeController : Controller
+    {
+        //set up the data context for the home controller class
+        Models.BlogEntities db = new Models.BlogEntities();
+
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View(db.Posts.OrderByDescending(x=> x.DateCreated));
+        }
+
+        public ActionResult AddComment(Models.Comment commentToAdd)
+        {
+            //make sure the comment is fully filled out
+            commentToAdd.DateCreated = DateTime.Now;
+
+            //add the comment to the database
+            db.Comments.Add(commentToAdd);
+            db.SaveChanges();
+
+            ////for now, until we ajax it, we will kick the user back
+            //// to the index
+            //return RedirectToAction("Index", "Home");
+
+            //ajax'd the POST method, so we will return 
+            // a partial view instead.  we must pass in
+            // the comment object that we just added
+            return PartialView("Comment", commentToAdd);
+        }
+
+        public ActionResult LikePost(int id)
+        {
+            //get the post from the database that 
+            // matches the id
+            Models.Post thePostToLike = db.Posts.Find(id);
+            //increment the likes by 1
+            thePostToLike.Likes++;
+            //save the changes to the database
+            db.SaveChanges();
+            //we need to return the number of likes
+            return Content(thePostToLike.Likes + " likes");
+        }
+
+    }
+}
---
+++ MyBlog-Class4/MyBlog-Class4/Scripts/main.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+    //show/hide the comments
+    $('.showComments').on('click', function () {
+        var parent = $(this).parent();
+        var commentsToShowOrHide = parent.find('.commentsDiv');
+        commentsToShowOrHide.slideToggle();
+    });
+    
+    //handle the Like button
+    $('.like').on('click', function () {
+        //what post does the user want to like?
+        var postID = $(this).data('postid');
+        //store the this element into a variable
+        var theLikeButton = $(this);
+        //do the AJAX get request to like the post
+        $.get('/Home/LikePost/' + postID, function (data) {
+            //update the HTML with the current number of likes
+            theLikeButton.parent().html(data);
+        });
+    });
+    
+    //wiring up the AJAX POST for the comment form
+    $('.commentsDiv form').on('submit', function (event) {
+        //prevent the form from posting normally
+        event.preventDefault();
+        var theForm = $(this);
+        $.post(theForm.attr('action'), theForm.serialize(), function (data) {
+            //update our html
+            theForm.before(data);
+            //clear out the values of the comment form
+            theForm.find('.commentAuthor, .commentBody').val('');
+        });
+        
+    });
+    
+});
